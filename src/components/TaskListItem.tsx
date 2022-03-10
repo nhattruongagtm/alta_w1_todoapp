f686c45af399c2a5e712becd1b77baa0dc40d2d8
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2022-03-10 19:37:08 +0700

    completed

--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -9,32 +9,50 @@ import { chooseItem, deleteTask, updateTask } from "../slide/TaskSlice";
 import { CheckboxChangeEvent } from "antd/lib/checkbox";
 
 interface Props {
-    task: Task;
+  task: Task;
 }
-const TaskListItem = ({task}: Props) => {
+const TaskListItem = ({ task }: Props) => {
   const { Text } = Typography;
   const dispatch = useDispatch();
-  const tasks = useSelector((state: RootState)=>state.tasks.taskList);
+  const tasks = useSelector((state: RootState) => state.tasks.taskList);
 
-  const handleEdit = () =>{
+  const handleEdit = () => {
     dispatch(chooseItem(task));
-  }
-  const handleDelete = () =>{
-    dispatch(deleteTask(task.id))
-  }
-  const handleChangeStatus = (e: CheckboxChangeEvent) =>{
+  };
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id));
+  };
+  const handleChangeStatus = (e: CheckboxChangeEvent) => {
     const checked = e.target.checked;
 
-    dispatch(updateTask({...task,status: checked}))
-
-  }
+    dispatch(updateTask({ ...task, status: checked }));
+  };
   return (
     <div className="task__list__item">
-      <Text className={task.status ? "task__item__content task--complete": "task__item__content"}>
-        <Checkbox checked={task.status} onChange={handleChangeStatus}></Checkbox> {task.title}  
+      <Text
+        className={
+          task.status
+            ? "task__item__content task--complete"
+            : "task__item__content"
+        }
+      >
+        <Checkbox
+          checked={task.status}
+          onChange={handleChangeStatus}
+        ></Checkbox>{" "}
+        {task.title}
       </Text>
-      <Button className="task__item__btn task--edit" onClick={handleEdit}>Sửa</Button>
-      <Button danger type="primary" className="task__item__btn task--delete" onClick={handleDelete}>Xóa</Button>
+      <Button className="task__item__btn task--edit" onClick={handleEdit}>
+        Sửa
+      </Button>
+      <Button
+        danger
+        type="primary"
+        className="task__item__btn task--delete"
+        onClick={handleDelete}
+      >
+        Xóa
+      </Button>
     </div>
   );
 };
